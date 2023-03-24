7443b083bce3f27ef4a5a42499c91cc15d34232c
George Osondu <browngeorge752@gmail.com>
2023-03-24 04:55:56 +0000

    smh, i am tired

--- print.c
+++ print.c
@@ -38,3 +38,44 @@ void pint(stack_t **stack, unsigned int lineNumber)
 	}
 	printf("%d\n", (*stack)->n);
 }
+
+
+/**
+ * pchar - Prints the ASCII value of the top element of the stack
+ * @stack: The stack
+ * @lineNumber: The current line number in the file
+ */
+void pchar(stack_t **stack, unsigned int lineNumber)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", lineNumber);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n",
+				lineNumber);
+		exit(EXIT_FAILURE);
+	}
+
+	putchar((*stack)->n);
+}
+
+/**
+ * pstr - Prints the string starting at the top of the stack
+ * @stack: The stack
+ * @lineNumber: The current line number in the file
+ */
+void pstr(stack_t **stack, unsigned int lineNumber)
+{
+	stack_t *current = *stack;
+
+	while (current != NULL && current->n > 0 && current->n < 128)
+	{
+		putchar(current->n);
+		current = current->next;
+	}
+	putchar('\n');
+}
